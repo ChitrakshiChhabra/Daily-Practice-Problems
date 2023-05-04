a8bc4ac20826e1b161a90697264c8d3372c4f561
Chitrakshi <chitrakshi.chhabra@gmail.com>
2023-05-04 21:46:54 +0530

    Day 5

---
+++ dota2Sonate.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+string predictPartyVictory(string senate)
+{
+
+    int score = 0; // +ve for R and -ve for D
+    for (int i = 0; i < senate.size(); ++i)
+    {
+        const char ch = senate[i];
+        if (ch == 'R')
+        {
+            if (score < 0)
+                senate.push_back('D');
+
+            ++score;
+        }
+        else
+        {
+            if (score > 0)
+                senate.push_back('R');
+
+            --score;
+        }
+    }
+    return score > 0 ? "Radiant" : "Dire";
+}
+int main()
+{
+    return 0;
+}
