647a229a0ec2e675fa2e81c402223c50bf17b367
Chitrakshi <chitrakshi.chhabra@gmail.com>
2023-05-15 20:46:39 +0530

    Day 16

---
+++ countingBits.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<int> countBits(int n)
+{
+    vector<int> ans;
+    for (int i = 0; i <= n; i++)
+    {
+        int sum = 0, num = i;
+        while (num != 0)
+        {
+            sum += num % 2;
+            num /= 2;
+        }
+        ans.push_back(sum);
+    }
+    return ans;
+}
+int main()
+{
+    return 0;
+}
