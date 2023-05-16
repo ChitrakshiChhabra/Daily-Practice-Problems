ba8f6fa7e1fb4f3a6f05f61fcd7c48766d94d6c9
Chitrakshi <chitrakshi.chhabra@gmail.com>
2023-05-16 22:27:22 +0530

    Day 17

---
+++ slowestKey.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+char slowestKey(vector<int> &releaseTimes, string keysPressed)
+{
+    int time = releaseTimes[0], new_time = 0;
+    char key = keysPressed[0];
+
+    for (int i = 1; i < releaseTimes.size(); i++)
+    {
+        new_time = releaseTimes[i] - releaseTimes[i - 1];
+
+        if (new_time == time)
+            key = keysPressed[i] > key ? keysPressed[i] : key;
+
+        else if (new_time > time)
+        {
+            time = new_time;
+            key = keysPressed[i];
+        }
+    }
+    return key;
+}
+int main()
+{
+    return 0;
+}
