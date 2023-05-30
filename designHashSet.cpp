2bd6215f7fdd73cd631c76b3a9934d5826fcedb5
Chitrakshi <chitrakshi.chhabra@gmail.com>
2023-05-30 20:45:27 +0530

    Day 31

---
+++ designHashSet.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class MyHashSet
+{
+public:
+    vector<int> s;
+
+    MyHashSet()
+    {
+
+        s.resize(1e6 + 1, 0);
+    }
+
+    void add(int key)
+    {
+
+        s[key] = 1;
+    }
+
+    void remove(int key)
+    {
+
+        s[key] = 0;
+    }
+
+    bool contains(int key)
+    {
+
+        return s[key];
+    }
+};
+int main()
+{
+    return 0;
+}
