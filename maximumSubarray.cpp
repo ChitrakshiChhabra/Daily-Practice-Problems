07b976cdfed525a36f3f913b7fbe34a98e4f4289
Chitrakshi <chitrakshi.chhabra@gmail.com>
2023-06-21 22:42:37 +0530

    Day 53

---
+++ maximumSubarray.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int maxSubArray(vector<int> &nums)
+{
+    int maxi = INT_MIN, sum = 0;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        sum += nums[i];
+        maxi = max(sum, maxi);
+        if (sum < 0)
+            sum = 0;
+    }
+
+    return maxi;
+}
+int main()
+{
+    return 0;
+}
