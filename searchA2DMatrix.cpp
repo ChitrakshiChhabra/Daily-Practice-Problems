a1ad3bcd3244edc34e17958ab47c4a63140b4462
Chitrakshi <chitrakshi.chhabra@gmail.com>
2023-08-07 21:34:12 +0530

    Day 100

---
+++ searchA2DMatrix.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool searchMatrix(vector<vector<int>> &matrix, int target)
+{
+
+    int i = 0, j = matrix[0].size() - 1;
+
+    while (i < matrix.size() && j >= 0)
+    {
+        if (matrix[i][j] == target)
+            return true;
+        else if (matrix[i][j] > target)
+            j -= 1;
+        else
+            i += 1;
+    }
+    return false;
+}
+
+int main()
+{
+    return 0;
+}
