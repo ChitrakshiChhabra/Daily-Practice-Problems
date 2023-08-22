8f24b28ebb3117e4d8ef5aa746191e3580a2009b
Chitrakshi <chitrakshi.chhabra@gmail.com>
2023-08-22 21:04:56 +0530

    Day 114

---
+++ excelSheetColumnTitle.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+string convertToTitle(int columnNumber)
+{
+    int n = columnNumber;
+    string ans;
+    while (n > 0)
+    {
+        n--;
+        auto [q, r] = div(n, 26);
+        ans.push_back(char('A' + r));
+        n = q;
+    }
+    reverse(ans.begin(), ans.end()); /
+    return ans;                      
+}
+
+int main()
+{
+    return 0;
+}
