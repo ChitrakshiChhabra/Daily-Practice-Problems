336b7e577fe189ece0e9888c6da8dea90214bb78
Chitrakshi <chitrakshi.chhabra@gmail.com>
2023-09-14 21:13:12 +0530

    Day 136

---
+++ longestPalendromicSubstring.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string ans = "";
+void expand(string &s, int left, int right)
+{
+    while (left >= 0 && right < s.size())
+    {
+        if (s[left] != s[right])
+            break;
+        left--, right++;
+    }
+    if (ans.size() < right - left)
+        ans = s.substr(left + 1, right - left - 1);
+}
+string longestPalindrome(string s)
+{
+    for (int i = 0; i < s.size(); i++)
+    {
+        expand(s, i, i);
+        expand(s, i, i + 1);
+    }
+    return ans;
+}
+
+int main()
+{
+    return 0;
+}
