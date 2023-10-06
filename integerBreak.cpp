346b39d3a7931ca4af96e6a2427b96eb1a475c87
Chitrakshi <chitrakshi.chhabra@gmail.com>
2023-10-06 19:57:51 +0530

    Day 158

---
+++ integerBreak.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int integerBreak(int n)
+{
+    if (n <= 3)
+        return n - 1;
+    int quotient = n / 3, remainder = n % 3;
+    if (remainder == 0)
+        return pow(3, quotient);
+    if (remainder == 1)
+        return pow(3, quotient - 1) * 4;
+    return pow(3, quotient) * 2;
+}
+
+int main()
+{
+    return 0;
+}
