d0a16260e720fb0a2d163de37b558c288c4a2967
Chitrakshi <chitrakshi.chhabra@gmail.com>
2023-10-25 19:34:45 +0530

    Day 177

---
+++ kThSymbolInGrammar.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int kthGrammar(int n, int k)
+{
+    if (n == 1)
+        return 0;
+    int length = 1 << (n - 2);
+    if (k <= length)
+        return kthGrammar(n - 1, k);
+    else
+        return 1 - kthGrammar(n - 1, k - length);
+}
+
+int main()
+{
+    return 0;
+}
