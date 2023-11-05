ade4fd56ff317a3d59b45379cc3547a3c3a9e6e4
Chitrakshi <chitrakshi.chhabra@gmail.com>
2023-11-05 20:14:41 +0530

    Day 188

---
+++ findTheWinnerOfAnArrayGame.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int getWinner(vector<int> &A, int k)
+{
+    int cur = A[0], win = 0;
+    for (int i = 1; i < A.size(); ++i)
+    {
+        if (A[i] > cur)
+        {
+            cur = A[i];
+            win = 0;
+        }
+        if (++win == k)
+            break;
+    }
+    return cur;
+}
+
+int main()
+{
+    return 0;
+}
