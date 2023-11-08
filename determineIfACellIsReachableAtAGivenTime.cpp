92f161e1e8f3a0a368c7a4421fdd3992be009bf0
Chitrakshi <chitrakshi.chhabra@gmail.com>
2023-11-08 20:27:54 +0530

    Day 191

---
+++ determineIfACellIsReachableAtAGivenTime.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+bool isReachableAtTime(int sx, int sy, int fx, int fy, int t)
+{
+    if (sx == fx && sy == fy && t == 1)
+        return false;
+    if (abs(sx - fx) <= t && abs(sy - fy) <= t)
+        return true;
+    return false;
+}
+
+int main()
+{
+
+    return 0;
+}
