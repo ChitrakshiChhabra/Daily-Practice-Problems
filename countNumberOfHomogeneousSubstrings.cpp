c8af3e7d2d74eca906a84b06078fd3b3d456ddff
Chitrakshi <chitrakshi.chhabra@gmail.com>
2023-11-09 14:00:51 +0530

    Day 192

---
+++ countNumberOfHomogeneousSubstrings.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int countHomogenous(string s)
+{
+    int ans = 0;
+    int currStreak = 0;
+    int MOD = 1e9 + 7;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (i == 0 || s[i] == s[i - 1])
+        {
+            currStreak++;
+        }
+        else
+        {
+            currStreak = 1;
+        }
+
+        ans = (ans + currStreak) % MOD;
+    }
+
+    return ans;
+}
+
+int main()
+{
+    return 0;
+}
